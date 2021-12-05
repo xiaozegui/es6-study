9f4df22ff1336824aef1406ac90f8ac1a9fe3b26
萧泽桂 <438349348@qq.com>
2021-12-05 22:09:52 +0800

    update git command

--- git_learn.js
+++ git_learn.js
@@ -25,6 +25,7 @@
     cmd17:git fetch origin master //抓取远程仓库更新，不合并
     cmd18:git pull orgin master //抓取远程仓库更新并合并
     //解决合并冲突:手动修改，都pull抓取更新
+    cmd19:git restore name
     */
 //保存文件执行 git add .
 
